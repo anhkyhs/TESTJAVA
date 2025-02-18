4f3f16b737283db8bf7fddb65a3b8af08d68a25c
truong.nguyen <nguyenxuantruong96.hust@gmail.com>
2025-02-18 15:02:06 +0700

    nop BTVN

---
+++ src/main/java/com/example/testjava/Main2.java
@@ -0,0 +1,36 @@
+package com.example.testjava;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập một chuỗi: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Chuỗi là đối xứng.");
+        } else {
+            System.out.println("Chuỗi không phải là đối xứng.");
+        }
+
+        scanner.close();
+    }
+
+    // Hàm kiểm tra chuỗi đối xứng
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // Nếu có ký tự không khớp, chuỗi không đối xứng
+            }
+            left++;
+            right--;
+        }
+
+        return true; // Nếu không có sự khác biệt, chuỗi là đối xứng
+    }
+}
---
+++ src/main/java/com/example/testjava/MainTest.java
@@ -0,0 +1,26 @@
+package com.example.testjava;
+
+public class MainTest {
+        public static void main(String[] args) {
+            int[] arr = {3, 5, 7, 2, 8, 1, 4};
+            int maxValue = findMax(arr);
+            System.out.println("Giá trị lớn nhất trong mảng là: " + maxValue);
+        }
+
+        public static int findMax(int[] arr) {
+            if (arr == null || arr.length == 0) {
+                throw new IllegalArgumentException("Mảng không được rỗng");
+            }
+
+            int max = arr[0]; // Giả sử phần tử đầu tiên là lớn nhất
+
+            // Duyệt qua mảng để tìm phần tử lớn nhất
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i]; // Cập nhật giá trị max nếu tìm thấy phần tử lớn hơn
+                }
+            }
+
+            return max;
+        }
+}
